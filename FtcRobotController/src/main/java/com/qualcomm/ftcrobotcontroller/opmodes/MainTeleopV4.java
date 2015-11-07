86d0f0c86181dc0b0c3a267d96fc75e5c9e7444d
Mky135 <mky.flores@hotmail.com>
2015-11-07 11:43:28 -0600

    began a V4 teleop to control arm motors

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainTeleopV4.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/MainTeleopV4.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.util.Range;
 /**
  * Created by cms_guest on 11/03/15.
  */
-public class MainTeleopV3 extends OpMode {
+public class MainTeleopV4 extends OpMode {
 
     //defines motors, RF means Right front, LR means Left Rear, etc.
     private DcMotor motorRF;
@@ -15,13 +15,17 @@ public class MainTeleopV3 extends OpMode {
     private DcMotor motorLF;
     private DcMotor motorLR;
     private DcMotor motorClimb;
+    private DcMotor arm1;
+    private DcMotor arm2;
 
+    //defines what what throttle, direction, left, right, etc. are (booleans, floats, etc.)
     private float throttle;
     private float direction;
     private float left;
     private float right;
 
-    public MainTeleopV3() {
+    //initiates variables so they equal zero or false, so when we start nothing conflicts
+    public MainTeleopV4() {
         throttle = 0;
         direction = 0;
         left = 0;
@@ -30,20 +34,25 @@ public class MainTeleopV3 extends OpMode {
 
     @Override
     public void init() {
+        //maps out motors so config knows where they are
         motorRF = hardwareMap.dcMotor.get("motorRF");
         motorRR = hardwareMap.dcMotor.get("motorRR");
         motorLF = hardwareMap.dcMotor.get("motorLF");
         motorLR = hardwareMap.dcMotor.get("motorLR");
         motorClimb = hardwareMap.dcMotor.get("motorClimb");
+        arm1 = hardwareMap.dcMotor.get("armOne");
+        arm2 = hardwareMap.dcMotor.get("armTwo");
     }
 
     @Override
     public void loop() {
+        //each time loop goes through, scans gamepads and assigns the values to their variables
         throttle = gamepad1.left_stick_y;
         direction = gamepad1.left_stick_x;
         right = throttle - direction;
         left = throttle + direction;
 
+        //takes the input and converts so it doesnt go over 1 or under -1
         right = Range.clip(right, -1, 1);
         left = Range.clip(left, -1, 1);
 
@@ -52,11 +61,38 @@ public class MainTeleopV3 extends OpMode {
         right = (float) -scaleInput(right);
         left = (float) scaleInput(left);
 
+        //sets motors to do what gathered input is
         motorRF.setPower(right);
         motorRR.setPower(right);
         motorLF.setPower(left);
         motorLR.setPower(left);
         motorClimb.setPower(-0.4);
+        arm1.setPower(0);
+        arm2.setPower(0);
+
+        if (gamepad2.left_trigger > 0) {
+            arm1.setPower(0.5);
+        } else {
+            arm1.setPower(0);
+        }
+
+        if (gamepad2.right_trigger > 0) {
+            arm2.setPower(0.5);
+        } else {
+            arm2.setPower(0);
+        }
+
+        if (gamepad2.left_bumper = true) {
+            arm1.setPower(-0.5);
+        } else {
+            arm1.setPower(0);
+        }
+
+        if (gamepad2.right_bumper = true) {
+            arm2.setPower(-0.5);
+        } else {
+            arm2.setPower(0);
+        }
     }
 
     /*
